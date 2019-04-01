f2747958ae76d24b68b7dbf3e9d609dc42924a07
Jorge <jorgepmartin@gmail.com>
2019-04-01 09:38:22 +0200

    First real commit with some hard-tests

---
+++ src/main/java/org/openmarkov/hardTesting/HardTesting.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) CISIAD, UNED, Spain,  2019. Licensed under the GPLv3 licence
+ * Unless required by applicable law or agreed to in writing,
+ * this code is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OF ANY KIND.
+ */
+
+package org.openmarkov.hardTesting;
+
+import org.openmarkov.full.OpenMarkov;
+
+public class HardTesting {
+
+	public static void main(String[] args) {
+		OpenMarkov.main(args);
+
+	}
+}
